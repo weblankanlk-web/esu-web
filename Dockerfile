f024ebac44e444064e175ea0fe252fd879e0ac8f
Lahiru Liyanage <75250151+weblankanteam@users.noreply.github.com>
2025-05-02 08:06:00 +0530

    update

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ FROM node:18-alpine
 WORKDIR /app
 
 COPY package.json package-lock.json ./
-RUN npm install
+#RUN npm install
+RUN npm install --legacy-peer-deps
+
 
 COPY . .
 
--- lib/graphql-client.ts
+++ lib/graphql-client.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from 'graphql-request';
 
-const endpoint = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://esoft.local/graphql';
+const endpoint = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'https://esu-cms.hostweblankan.in/graphql';
 
 export const graphQLClient = new GraphQLClient(endpoint);
