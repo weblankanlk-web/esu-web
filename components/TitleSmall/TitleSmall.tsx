53a49465b29bbf2e7bed130808b055b9ff8952f7
Anuja Lamahewa <manager2@weblankan.com>
2025-05-09 05:47:34 +0530

    Update TitleSmall.tsx

--- components/TitleSmall/TitleSmall.tsx
+++ components/TitleSmall/TitleSmall.tsx
@@ -15,7 +15,8 @@ const TitleSmall: React.FC<Title> = ({ title, subtitle }) => {
     <>
       <div className="title-wrapper">
         <div className="main-title-small">
-          <h2> { title }
+          <h2> { title } </h2>
+          <h2>
           <span style={{ color: color }}> {subtitle }</span>
           </h2>
         </div>
