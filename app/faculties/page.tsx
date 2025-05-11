f8d1781597f7a312beb844cdce1da46aff31b8fb
Anuja Lamahewa <manager2@weblankan.com>
2025-05-11 05:36:30 +0530

    updated the title and the text of home page

--- app/faculties/page.tsx
+++ app/faculties/page.tsx
@@ -30,8 +30,7 @@ const Page = () => {
     <>
       <InnerBanner
         innerPageTitle={`Our <span>Faculties</span>`}
-        innerPageDescription="Where we breathe life into technology! 
-        Welcome to ESOFT Metro Campus, where seven dynamic schools shape your educational journey. Join us in this diverse academic landscape for unparalleled excellence!"
+        innerPageDescription=""
         innerBgDesk="/images/faculity-lan.png"
         innerBgMobi="/images/faculity-lan.png"
       />
