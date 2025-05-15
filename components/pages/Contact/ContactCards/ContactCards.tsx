1419413cbe05628477c7bdbdb4836385a0e57af3
gayanweblankan <dev3@weblankan.com>
2025-05-15 13:40:02 +0530

    contact branches

--- components/pages/Contact/ContactCards/ContactCards.tsx
+++ components/pages/Contact/ContactCards/ContactCards.tsx
@@ -3,96 +3,124 @@ import "./style.scss";
 
 const ContactCards = () => {
   return (
-    <div className="contact-cards">
-      <div className="card">
-        <h3>
-          <span className="highlight">Colombo</span>
-        </h3>
-        <p className="label">Address</p>
-        <p>Head Office (Block E)<br></br>
-No.03,<br></br>
-De Fonseka Place, Colombo 4,<br></br> Sri Lanka.</p>
-        <div className="info">
-          <div>
-            <p className="label">Email</p>
-            <p>info@esu.lk</p>
-          </div>
-          <div>
-            <p className="label">Call Us on</p>
-            <p>+94 117 572 572</p>
-          </div>
+    <>
+ 
+
+<section className="branches-section">
+  <div className="branches-section-wrap">
+    <div className="single-branch">
+      <h3 className="branch-title">Colombo</h3>
+      <div className="branch-details">
+        <div className="address">
+          <p className="label">Address</p>
+          <p>Head Office (Block E)<br></br>
+            No.03,<br></br>
+            De Fonseka Place, Colombo 4,<br></br> Sri Lanka.</p>
+        </div>
+        <div className="email">
+          <p className="label">Email</p>
+          <a href="mailto:info@esu.lk">info@esu.lk</a>
+        </div>
+        <div className="call-us">
+          <p className="label">Call Us on</p>
+          <a href="tel:+94 117 572 572" className="">+94 117 572 572</a>
         </div>
       </div>
-
-      <div className="card">
-        <h3>
-          <span className="highlight">Kandy</span>
-        </h3>
-        <p className="label">Address</p>
-        <p>
-          No 479,
-          <br />
-          William Gopallawa mawatha, <br />
-          Kandy,<br></br> Srilanka
-        </p>
-        <div className="info">
-          <div>
-            <p className="label">Call Us on</p>
-            <p>+94 117 572 572</p>
-          </div>
-          <div>
-            <p className="label">Email</p>
-            <p>info@esu.lk</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="card">
-        <h3>
-          <span className="highlight">Colombo</span>
-        </h3>
-        <p className="label">Address</p>
-        <p>
+    </div>
+    <div className="single-branch">
+      <h3 className="branch-title">Colombo</h3>
+      <div className="branch-details">
+        <div className="address">
+          <p className="label">Address</p>
+          <p>
           Block A
           <br />
           235, Galle Road <br />
           Colombo 04,<br></br> Srilanka
         </p>
-        <div className="info">
-          <div>
-            <p className="label">Call Us on</p>
-            <p>+94 117 572 572</p>
-          </div>
-          <div>
-            <p className="label">Email</p>
-            <p>info@esu.lk</p>
-          </div>
+        </div>
+        <div className="email">
+          <p className="label">Email</p>
+          <a href="mailto:info@esu.lk">info@esu.lk</a>
+        </div>
+        <div className="call-us">
+          <p className="label">Call Us on</p>
+          <a href="tel:+94 117 572 572" className="">+94 117 572 572</a>
         </div>
       </div>
-
-      <div className="card">
-        <h3>
-          <span className="highlight">Colombo</span>
-        </h3>
-        <p className="label">Address</p>
-        <p>
+    </div>
+     <div className="single-branch">
+      <h3 className="branch-title">Colombo</h3>
+      <div className="branch-details">
+        <div className="address">
+          <p className="label">Address</p>
+          <p>
           Block B
           <br />
           233, Galle Road <br />
           Colombo 04,<br></br> Srilanka
         </p>
-        <div className="info">
-          <div>
-            <p className="label">Call Us on</p>
-            <p>+94 117 572 572</p>
-          </div>
-          <div>
-            <p className="label">Email</p>
-            <p>info@esu.lk</p>
-          </div>
+        </div>
+        <div className="email">
+          <p className="label">Email</p>
+          <a href="mailto:info@esu.lk">info@esu.lk</a>
+        </div>
+        <div className="call-us">
+          <p className="label">Call Us on</p>
+          <a href="tel:+94 117 572 572" className="">+94 117 572 572</a>
         </div>
       </div>
     </div>
+    <div className="single-branch">
+      <h3 className="branch-title">Kandy</h3>
+      <div className="branch-details">
+        <div className="address">
+          <p className="label">Address</p>
+           <p>
+          No 479,
+          <br />
+          William Gopallawa mawatha, <br />
+          Kandy,<br></br> Srilanka
+        </p>
+        </div>
+        <div className="email">
+          <p className="label">Email</p>
+          <a href="mailto:info@esu.lk">info@esu.lk</a>
+        </div>
+        <div className="call-us">
+          <p className="label">Call Us on</p>
+          <a href="tel:+94 117 572 572" className="">+94 117 572 572</a>
+        </div>
+      </div>
+    </div>
+    {/* <div className="single-branch">
+      <h3 className="branch-title">Kandy</h3>
+      <div className="branch-details">
+        <div className="address">
+          <p className="label">Address</p>
+           <p>
+          No 479,
+          <br />
+          William Gopallawa mawatha, <br />
+          Kandy,<br></br> Srilanka
+        </p>
+        </div>
+        <div className="email">
+          <p className="label">Email</p>
+          <a href="mailto:info@esu.lk">info@esu.lk</a>
+        </div>
+        <div className="call-us">
+          <p className="label">Call Us on</p>
+          <a href="tel:+94 117 572 572" className="">+94 117 572 572</a>
+        </div>
+      </div>
+    </div> */}
+  </div>
+</section>
+
+    
+    </>
+   
   );
 };
 
