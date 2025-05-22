1fb9bb968fb17791fa91ca59d3e3659ffe6540a1
InsafInhaamDev <dev@weblankan.com>
2025-05-23 00:18:01 +0530

    css new inner change

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -52,7 +52,7 @@ const page = () => {
     <>
       <Breadrumb />
 
-      <section className="simple-padding-bottom research-inner-page">
+      <section className="simple-padding-bottom news-inner-page">
         <div className="small-middle-wrap">
           <div className="heading-wrap d-flex small-wrap">
             <h2 className="section-heading section-heading--black section-heading--underline section-heading--underline--center">
