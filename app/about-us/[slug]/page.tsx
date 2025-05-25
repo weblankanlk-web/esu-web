89599b58ae429d4315a5fbb8d768a717e8d44266
Bavaram <karunaharan.bavaram@weblankan.lk>
2025-05-25 18:45:04 +0530

    changes

--- app/about-us/[slug]/page.tsx
+++ app/about-us/[slug]/page.tsx
@@ -103,7 +103,7 @@ const page = () => {
 
       <CampusFacilities />
 
-      <CreativeCollage slug={slug || ""} />
+      {/* <CreativeCollage slug={slug || ""} /> */}
 
       <ContactHeadOffice />
    
