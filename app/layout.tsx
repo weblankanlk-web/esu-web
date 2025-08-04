8ae1ef85b087396d65760e69b50770e62450dc70
InsafInhaamDev <dev@weblankan.com>
2025-08-04 12:29:58 +0530

    favicons added all sizes

--- app/layout.tsx
+++ app/layout.tsx
@@ -102,7 +102,20 @@ export default function RootLayout({
             });
           `}
         </Script>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body
         className={`
